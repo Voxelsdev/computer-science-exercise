6da0377f9afc68b58da5634d9cf3cff2ff494eda
Tyler Miller <tylermillerhome@gmail.com>
2016-12-13 10:53:35 -0800

    day1 donw

--- algorithms/search_algorithms.js
+++ algorithms/search_algorithms.js
@@ -1,11 +1,33 @@
 'use strict'
 
 const linearSearch = function(array, value) {
-
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === value) {
+      return i;
+    }
+  }
+  return -1;
 }
 
-const binarySearch = function(array, value) {
+let binarySearch = function(arr, val) {
+  if (arr.length) {
+    let min = 0;
+    let max = arr.length;
+    let mid = Math.floor((max + min) / 2);
 
+    while (min <= max) {
+      if (arr[mid] < val) {
+        min = mid + 1;
+      } else if (arr[mid] > val) {
+        max = mid - 1;
+      } else {
+        return mid;
+      }
+
+      mid = Math.floor((max + min) / 2);
+    }
+  }
+  return -1;
 }
 
 module.exports = { linearSearch, binarySearch };
